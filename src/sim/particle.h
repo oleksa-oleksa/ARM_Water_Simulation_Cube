d29fdd82151e62c08ef23f3c890cd79effb5d924
Heiko Radde <heiko-rudow@live.de>
2018-10-25 13:42:24 +0200

    src/sim: added particle structures, added initialisation functions, added move-functions

---
+++ src/sim/particle.h
@@ -0,0 +1,44 @@
+#ifndef PARTICLE_H_
+#define PARTICLE_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define PARTICLE_MASS       50
+#define PARTICLE_DENSITY    12
+#define PARTICLE_PREASURE   10
+
+#define PARTICLE_GRID_X     32
+#define PARTICLE_GRID_Y     32
+
+
+typedef struct
+{
+    double position[2];
+    double velocity[2];
+    double mass;
+    double density;
+    double preasure;
+    double force[2];
+    //color
+} particle_t;
+
+typedef struct particle_list_element_s
+{
+    particle_t* particle;
+    struct particle_list_element_s* prev;
+    struct particle_list_element_s* next;
+} particle_list_element_t;
+
+typedef struct
+{
+    particle_list_element_t* particle_list;
+    uint32_t particle_count;
+} particle_grid_element_t;
+
+
+bool init_particle_list(particle_list_element_t* list, particle_t* particle_array, uint32_t size);
+
+bool init_particle_grid(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], particle_list_element_t* list, uint32_t size);
+
+#endif
