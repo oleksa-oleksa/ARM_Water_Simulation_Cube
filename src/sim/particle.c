f3d0cfbafd07867ead50dc9f9c858bd9fa8d072b
Heiko Radde <heiko-rudow@live.de>
2018-11-08 14:45:25 +0100

    src/sim: continued math-implementation: added calculation of foced acting upon any one particle

--- src/sim/particle.c
+++ src/sim/particle.c
@@ -10,13 +10,16 @@
 
 #define REST_DENS 1000.0    ///< rest density
 #define GAS_CONST 2000.0    ///< gas constant for euation of state
+#define VISCOSITY 250.0     ///< viscosity
 
 #define X 0
 #define Y 1
 
 void _compute_density_pressure(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y]);
-inline double _kernel_smoothing(double distance);
+inline double _kernel_smoothing_density(double distance);
 void _compute_forces(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double ex_force[2]);
+inline double _kernel_smoothing_pressure(double distance);
+inline double _kernel_smoothing_viscosity(double distance);
 void _integrate(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y]);
 
 
@@ -185,7 +188,7 @@ bool particle_move(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y
 void _compute_density_pressure(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y])
 {
     int i_x, i_y, d_x, d_y;
-    uint32_t i_part, k_part;
+    uint32_t i_part, j_part;
     particle_list_element_t* curr;
 
     for(i_x = 0; i_x < PARTICLE_GRID_X; ++i_x)
@@ -199,23 +202,23 @@ void _compute_density_pressure(particle_grid_element_t grid[PARTICLE_GRID_X][PAR
             {
                 curr = &grid[i_x][i_y].particle_list[i_part];
                 curr->particle.pressure = 0;
-                //now use the particles within the smoothing-distance (surounding grid-cells) to calculate the densitiy at the current particle
-                for(d_x = PARTICLE_GRID_SMOOTH_DISTANCE * (-1); d_x <= PARTICLE_GRID_SMOOTH_DISTANCE; ++d_x)
+                //now use the particles within the smoothing-distance (surounding grid-cells) to calculate the density at the current particle
+                for(d_x = pow(PARTICLE_GRID_SMOOTH_DISTANCE, 2) * (-1); d_x <= pow(PARTICLE_GRID_SMOOTH_DISTANCE, 2); ++d_x)
                 {
                     if( (i_x + d_x > 0) && 
                         (i_x + d_x < PARTICLE_GRID_X) )
                     {
-                        for(d_y = PARTICLE_GRID_SMOOTH_DISTANCE * (-1); d_y <= PARTICLE_GRID_SMOOTH_DISTANCE; ++d_y)
+                        for(d_y = pow(PARTICLE_GRID_SMOOTH_DISTANCE, 2) * (-1); d_y <= pow(PARTICLE_GRID_SMOOTH_DISTANCE, 2); ++d_y)
                         {
                             if( (i_y + d_y > 0) && 
                                 (i_y + d_y < PARTICLE_GRID_Y) )
                             {
-                                for(k_part = 0; k_part < grid[i_x+d_x][i_y+d_y].particle_count; ++k_part)
+                                for(j_part = 0; j_part < grid[i_x+d_x][i_y+d_y].particle_count; ++j_part)
                                 {
-                                    //densitiy is sum of: particlemass * kernel-smoothing
-                                    double distance = sqrt( pow(curr->particle.position[X] - grid[i_x+d_x][i_y+d_y].particle_list[k_part].particle.position[X], 2) +  
-                                                            pow(curr->particle.position[Y] - grid[i_x+d_x][i_y+d_y].particle_list[k_part].particle.position[Y], 2) );
-                                    curr->particle.density += grid[i_x+d_x][i_y+d_y].particle_list[k_part].particle.mass * _kernel_smoothing(distance);
+                                    //density is sum of: particlemass * kernel-smoothing
+                                    double distance = sqrt( pow(curr->particle.position[X] - grid[i_x+d_x][i_y+d_y].particle_list[j_part].particle.position[X], 2) +  
+                                                            pow(curr->particle.position[Y] - grid[i_x+d_x][i_y+d_y].particle_list[j_part].particle.position[Y], 2) );
+                                    curr->particle.density += grid[i_x+d_x][i_y+d_y].particle_list[j_part].particle.mass * _kernel_smoothing_density(distance) * pow(pow(PARTICLE_GRID_SMOOTH_DISTANCE, 2) - pow(distance, 2), 3.0);
                                 }
                             }
                         }
@@ -231,7 +234,7 @@ void _compute_density_pressure(particle_grid_element_t grid[PARTICLE_GRID_X][PAR
 }
 
 
-double _kernel_smoothing(double distance)
+double _kernel_smoothing_density(double distance)
 {
     //poly6 kernel funciton
     return 315.0 / (65.0 * M_PI * pow(distance, 9.0));
@@ -240,8 +243,83 @@ double _kernel_smoothing(double distance)
 
 void _compute_forces(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double ex_force[2])
 {
-    (void)grid;
-    (void)ex_force;
+    int i_x, i_y, d_x, d_y;
+    uint32_t i_part, j_part;
+    particle_list_element_t* curr_i;
+    particle_list_element_t* curr_j;
+    double f_pressure[2];
+    double f_viscosity[2];
+    double f_gravity[2];
+
+    for(i_x = 0; i_x < PARTICLE_GRID_X; ++i_x)
+    {
+        for(i_y = 0; i_y < PARTICLE_GRID_Y; ++i_y)
+        {
+            //get a particle and calculate the force applied to it depending on it's neighbours
+            for(i_part = 0; i_part < grid[i_x][i_y].particle_count; ++i_part)
+            {
+                curr_i = &grid[i_x][i_y].particle_list[i_part];
+                f_pressure[X] = 0;
+                f_pressure[Y] = 0;
+                f_viscosity[X] = 0;
+                f_viscosity[Y] = 0;
+                f_gravity[X] = ex_force[X] * curr_i->particle.density;
+                f_gravity[Y] = ex_force[Y] * curr_i->particle.density;
+                for(d_x = PARTICLE_GRID_SMOOTH_DISTANCE * (-1); d_x <= PARTICLE_GRID_SMOOTH_DISTANCE; ++d_x)
+                {
+                    if( (i_x + d_x > 0) && 
+                        (i_x + d_x < PARTICLE_GRID_X) )
+                    {
+                        for(d_y = PARTICLE_GRID_SMOOTH_DISTANCE * (-1); d_y <= PARTICLE_GRID_SMOOTH_DISTANCE; ++d_y)
+                        {
+                            if( (i_y + d_y > 0) && 
+                                (i_y + d_y < PARTICLE_GRID_Y) )
+                            {
+                                for(j_part = 0; j_part < grid[i_x+d_x][i_y+d_y].particle_count; ++j_part)
+                                {
+                                    curr_j = &grid[i_x+d_x][i_y+d_y].particle_list[j_part];
+                                    //density is sum of: particlemass * kernel-smoothing
+                                    
+                                    double v_ij[2]; //vector between i and j of i and j
+                                    v_ij[X] = curr_j->particle.position[X] - curr_i->particle.position[X];
+                                    v_ij[Y] = curr_j->particle.position[Y] - curr_i->particle.position[Y];
+                                    double distance = sqrt( pow(curr_i->particle.position[X] - grid[i_x+d_x][i_y+d_y].particle_list[j_part].particle.position[X], 2) +  
+                                                            pow(curr_i->particle.position[Y] - grid[i_x+d_x][i_y+d_y].particle_list[j_part].particle.position[Y], 2) );
+                                    double x_normalized = 0;
+                                    double y_normalized = 0;
+                                    if(distance > 0)
+                                    {
+                                        x_normalized = v_ij[X] / distance;
+                                        y_normalized = v_ij[Y] / distance;
+                                    }
+                                    double multiplicator = curr_j->particle.mass * (curr_i->particle.pressure + curr_j->particle.pressure) / (2.0 * curr_j->particle.density) * _kernel_smoothing_pressure(distance);
+                                    f_pressure[X]  += x_normalized * multiplicator;
+                                    f_pressure[Y]  += y_normalized * multiplicator;
+                                    f_viscosity[X] += VISCOSITY * curr_j->particle.mass * (curr_j->particle.velocity[X] - curr_i->particle.velocity[X]) / curr_j->particle.density * _kernel_smoothing_viscosity(distance);
+                                }
+                            }
+                        }
+                    }
+                }
+                curr_i->particle.force[X] = f_pressure[X] + f_viscosity[X] + f_gravity[X];
+                curr_i->particle.force[Y] = f_pressure[Y] + f_viscosity[Y] + f_gravity[Y];
+            }
+        }
+    }
+}
+
+
+double _kernel_smoothing_pressure(double distance)
+{
+    //spiky gradient kernel (constant scalar)
+    return (-45.0) / (M_PI * pow(PARTICLE_GRID_SMOOTH_DISTANCE, 6.0))  * pow(PARTICLE_GRID_SMOOTH_DISTANCE - distance, 2.0);
+}
+
+
+double _kernel_smoothing_viscosity(double distance)
+{
+    //viscosity kernel (laplacian scalar)
+    return (45) / (M_PI * pow(PARTICLE_GRID_SMOOTH_DISTANCE, 6.0)) * (PARTICLE_GRID_SMOOTH_DISTANCE - distance);
 }
 
 
