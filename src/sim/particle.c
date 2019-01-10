06793cda81185caad22f2f7399af56b0622669ec
Heiko Radde <s76526@beuth-hochschule.de>
2019-01-10 11:11:18 +0100

    src/sim: started port of simulation from single-panel to cube

--- src/sim/particle.c
+++ src/sim/particle.c
@@ -30,6 +30,7 @@ void _compute_forces(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID
 inline double _kernel_smoothing_pressure(double distance);
 inline double _kernel_smoothing_viscosity(double distance);
 void _integrate(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double d_time);
+void _integrate_cube(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double d_time);
 
 
 
@@ -47,18 +48,12 @@ static inline void _update_grid_position(particle_grid_element_t grid[PARTICLE_G
     if((new_grid_x != orig_grid_x) ||
        (new_grid_y != orig_grid_y))
     {
-        // printf("%p [%2d][%2d] ->", (void*)element, orig_grid_x, orig_grid_y);
-        // printf(" [%2d][%2d]\r\n", new_grid_x, new_grid_y);
         //remove element from old grid-cell
-        // printf("[%d][%d] %"PRIu32"\r\n", orig_grid_x, orig_grid_y, grid[orig_grid_x][orig_grid_y].particle_count);
         curr = grid[orig_grid_x][orig_grid_y].particle_list;
         for(i = 0; i < grid[orig_grid_x][orig_grid_y].particle_count; ++i)
         {
-            // printf("%p\r\n", (void*)curr);
             if(curr == element)
             { //found the element to remove
-                // printf("position %"PRIu32" in particle_list\r\n", i);
-                // printf("%p %p %p\r\n", (void*)element->next, (void*)element->prev, (void*)grid[orig_grid_x][orig_grid_y].particle_list);
                 grid[orig_grid_x][orig_grid_y].particle_count -= 1;
                 if(grid[orig_grid_x][orig_grid_y].particle_count == 0)
                 {
@@ -83,41 +78,110 @@ static inline void _update_grid_position(particle_grid_element_t grid[PARTICLE_G
                         ;
                     }
                 }
-                // printf("%p %p %p\r\n", (void*)element->next, (void*)element->prev, (void*)grid[orig_grid_x][orig_grid_y].particle_list);
-                // printf("[%d][%d] %"PRIu32"\r\n", orig_grid_x, orig_grid_y, grid[orig_grid_x][orig_grid_y].particle_count);
-                // printf("removed element\r\n");
-                //break;
             }
             curr = curr->next;
         }
         element->next = NULL;
         element->prev = NULL;
         //add element to new grid-cell
-        // printf("%"PRIu32" elements in new grid-cell\r\n", grid[new_grid_x][new_grid_y].particle_count);
         if(grid[new_grid_x][new_grid_y].particle_count != 0)
         {
             curr = grid[new_grid_x][new_grid_y].particle_list;
-            // printf("curr = %p\r\n", (void*)curr);
             for(i = 1; i < grid[new_grid_x][new_grid_y].particle_count; ++i)
             {
                 curr = curr->next;
             }
-            // printf("curr = %p\r\n", (void*)curr);
             curr->next = element;
             element->prev = curr;
             element->next = NULL;
         }
         else
         {
-            // printf("first element in new grid\r\n");
             grid[new_grid_x][new_grid_y].particle_list = element;
             element->prev = element->next = NULL;
         }
         grid[new_grid_x][new_grid_y].particle_count += 1;
-        // printf("[%d][%d] %"PRIu32"\r\n", new_grid_x, new_grid_y, grid[new_grid_x][new_grid_y].particle_count);
-        // printf("%p %p %p\r\n", (void*)element->next, (void*)element->prev, (void*)grid[new_grid_x][new_grid_y].particle_list);
-        // printf("done moving\r\n\r\n");
     }
+}
+
+static inline void _update_cube_position(particle_grid_element_t top[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t bottom[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t front[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t back[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t left[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t right[PARTICLE_GRID_X][PARTICLE_GRID_Y])
+{
+    (void)top;
+    (void)bottom;
+    (void)front;
+    (void)back;
+    (void)left;
+    (void)right;
+    // int new_grid_x, new_grid_y;
+    // uint32_t i;
+    // particle_list_element_t* curr;
+
+    // new_grid_x = element->particle.position[X] / PARTICLE_GRID_CELL_WIDTH;   //c always cuts off decimal places double to int
+    // new_grid_y = element->particle.position[Y] / PARTICLE_GRID_CELL_HEIGHT;  //c always cuts off decimal places double to int
+
+
+    // if((new_grid_x != orig_grid_x) ||
+    //    (new_grid_y != orig_grid_y))
+    // {
+    //     //remove element from old grid-cell
+    //     curr = grid[orig_grid_x][orig_grid_y].particle_list;
+    //     for(i = 0; i < grid[orig_grid_x][orig_grid_y].particle_count; ++i)
+    //     {
+    //         if(curr == element)
+    //         { //found the element to remove
+    //             grid[orig_grid_x][orig_grid_y].particle_count -= 1;
+    //             if(grid[orig_grid_x][orig_grid_y].particle_count == 0)
+    //             {
+    //                 grid[orig_grid_x][orig_grid_y].particle_list = NULL;
+    //             }
+    //             else
+    //             {
+    //                 if(element->prev != NULL)
+    //                 {
+    //                     element->prev->next = element->next;
+    //                 }
+    //                 else
+    //                 {//element was the first of the particle_list
+    //                     grid[orig_grid_x][orig_grid_y].particle_list = element->next;
+    //                 }
+    //                 if(element->next != NULL)
+    //                 {
+    //                     element->next->prev = element->prev;
+    //                 }
+    //                 else
+    //                 {//element was the last of the particle_list
+    //                     ;
+    //                 }
+    //             }
+    //         }
+    //         curr = curr->next;
+    //     }
+    //     element->next = NULL;
+    //     element->prev = NULL;
+    //     //add element to new grid-cell
+    //     if(grid[new_grid_x][new_grid_y].particle_count != 0)
+    //     {
+    //         curr = grid[new_grid_x][new_grid_y].particle_list;
+    //         for(i = 1; i < grid[new_grid_x][new_grid_y].particle_count; ++i)
+    //         {
+    //             curr = curr->next;
+    //         }
+    //         curr->next = element;
+    //         element->prev = curr;
+    //         element->next = NULL;
+    //     }
+    //     else
+    //     {
+    //         grid[new_grid_x][new_grid_y].particle_list = element;
+    //         element->prev = element->next = NULL;
+    //     }
+    //     grid[new_grid_x][new_grid_y].particle_count += 1;
+    // }
 }
 
 
@@ -140,21 +204,6 @@ bool particle_init_list(particle_list_element_t* list, uint32_t size)
         list[i].particle.force[0]    = 0;
         list[i].particle.force[1]    = 0;
         list[i].next = list[i].prev = NULL;
-        // if(i == 0)
-        // {
-        //     list[i].prev = NULL;
-        //     list[i].next = &list[i+1];
-        // }
-        // if(i == size - 1)
-        // {
-        //     list[i].prev = &list[i-1];
-        //     list[i].next = NULL;
-        // }
-        // else
-        // {
-        //     list[i].prev = &list[i-1];
-        //     list[i].next = &list[i+1];
-        // }
     }
     return true;
 }
@@ -178,7 +227,6 @@ bool particle_init_grid(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_G
     {
         particle_x = (int)list[i].particle.position[X] / PARTICLE_GRID_CELL_WIDTH;
         particle_y = (int)list[i].particle.position[Y] / PARTICLE_GRID_CELL_HEIGHT;
-        // printf("%2"PRIu32": %5.2lf %5.2lf\n", i, list[i].particle.position[X], list[i].particle.position[Y]);
         if(grid[particle_x][particle_y].particle_count == 0)
         {
             grid[particle_x][particle_y].particle_count = 1;
@@ -188,9 +236,6 @@ bool particle_init_grid(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_G
         }
         else
         {
-            // _move_list_element(&list[i], 
-            //                    &grid[particle_x][particle_y].particle_list[ 
-            //                     grid[particle_x][particle_y].particle_count - 1]);
             curr = grid[particle_x][particle_y].particle_list;
             for(j = 1; j < grid[particle_x][particle_y].particle_count; ++j)
             {
@@ -206,7 +251,7 @@ bool particle_init_grid(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_G
 }
 
 
-bool particle_move(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double d_time, double ex_force[2])
+bool particle_move_single_panel(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double d_time, double ex_force[2])
 {
     int i_x, i_y;
     uint32_t part;
@@ -224,46 +269,48 @@ bool particle_move(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y
             for(part = 0; part < grid[i_x][i_y].particle_count; ++part)
             {
                 following = curr->next;
-                // printf("[%2d][%2d] %2d(%2d)"PRIu32" %p\r\n", i_x, i_y, part, grid[i_x][i_y].particle_count-1, (void*)curr);
-                // printf("next: %p\r\n", (void*)curr->next);
                 _update_grid_position(grid, i_x, i_y, curr);
                 curr = following;
             }
         }
     }
-    // printf("---------------------------------\r\n");
-
-    // for(i_y = 0; i_y < PARTICLE_GRID_X; ++i_y)
-    // {
-    //     for(i_x = 0; i_x < PARTICLE_GRID_Y; ++i_x)
-    //     {
-    //         printf("%4"PRIu32" ", grid[i_x][i_y].particle_count);
-    //     }
-    //     printf("\r\n");
-    // }
-    // for(i_y = 0; i_y < PARTICLE_GRID_X; ++i_y)
-    // {
-    //     for(i_x = 0; i_x < PARTICLE_GRID_Y; ++i_x)
-    //     {
-    //         printf("%4"PRIu32" ", grid[i_x][i_y].particle_count);
-    //     }
-    //     printf("\r\n");
-    //     for(i_x = 0; i_x < PARTICLE_GRID_Y; ++i_x)
-    //     {
-    //         if(grid[i_x][i_y].particle_count > 0)
-    //         {
-    //             uint64_t k = (uint64_t)grid[i_x][i_y].particle_list;
-    //             printf("%4"PRIX16" ", (uint16_t)k);
-    //         }
-    //         else
-    //         {
-    //             printf("____ ");
-    //         }
-    //     }
-    //     printf("\r\n");
-    // }
     return true;
 }
+
+
+bool particle_move_cube(particle_grid_element_t top[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t bottom[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t front[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t back[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t left[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        particle_grid_element_t right[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
+                        double d_time, double ex_force[2])
+{
+    _compute_density_pressure(top);
+    _compute_forces(top, ex_force);
+    _compute_density_pressure(bottom);
+    _compute_forces(bottom, ex_force);
+    _compute_density_pressure(front);
+    _compute_forces(front, ex_force);
+    _compute_density_pressure(back);
+    _compute_forces(back, ex_force);
+    _compute_density_pressure(left);
+    _compute_forces(left, ex_force);
+    _compute_density_pressure(right);
+    _compute_forces(right, ex_force);
+
+    _integrate_cube(top, d_time);
+    _integrate_cube(bottom, d_time);
+    _integrate_cube(front, d_time);
+    _integrate_cube(back, d_time);
+    _integrate_cube(left, d_time);
+    _integrate_cube(right, d_time);
+
+    _update_cube_position(top, bottom, front, back, left, right);
+    
+    return true;
+}
+
 
 
 
@@ -285,7 +332,6 @@ void _compute_density_pressure(particle_grid_element_t grid[PARTICLE_GRID_X][PAR
             for(i_part = 0; i_part < grid[i_x][i_y].particle_count; ++i_part)
             {
                 curr->particle.density = 0;
-                // printf("[%2d][%2d] no. %"PRIu32": density  = %lf\r\n", i_x, i_y, i_part, curr->particle.density);
                 //now use the particles within the smoothing-distance (surounding grid-cells) to calculate the density at the current particle
                 for(d_x = KERNEL_RADIUS_SQ * (-1); d_x <= KERNEL_RADIUS_SQ; ++d_x)
                 {
@@ -306,9 +352,6 @@ void _compute_density_pressure(particle_grid_element_t grid[PARTICLE_GRID_X][PAR
                                     if(pow(distance, 2) < KERNEL_RADIUS_SQ)
                                     {
                                         curr->particle.density += partner->particle.mass * _kernel_smoothing_density(distance);
-                                        // printf("distance = %lf\r\n", distance);
-                                        // printf("mass = %lf, smoothing = %lf\r\n", grid[i_x+d_x][i_y+d_y].particle_list[j_part].particle.mass, _kernel_smoothing_density(distance));
-                                        // printf("[%2d][%2d] no. %"PRIu32": density  = %lf\r\n", i_x, i_y, i_part, curr->particle.density);
                                     }
                                     partner = partner->next;
                                 }
@@ -319,9 +362,6 @@ void _compute_density_pressure(particle_grid_element_t grid[PARTICLE_GRID_X][PAR
                 //now we can use the density at the current particle to calculate the pressure at the current particle
                 // P = k_p * (rho - rho_0)
                 curr->particle.pressure = GAS_CONST * (curr->particle.density - REST_DENS);
-                // printf("[%2d][%2d] no. %"PRIu32": density  = %lf\r\n", i_x, i_y, i_part, curr->particle.density);
-                // printf("[%2d][%2d] no. %"PRIu32": pressure = %lf\r\n", i_x, i_y, i_part, curr->particle.pressure);
-                // printf("---------------------------------------\r\n");
                 curr = curr->next;
             }
         }
@@ -399,8 +439,6 @@ void _compute_forces(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID
                 }
                 curr->particle.force[X] = f_pressure[X] + f_viscosity[X] + f_gravity[X];
                 curr->particle.force[Y] = f_pressure[Y] + f_viscosity[Y] + f_gravity[Y];
-                // printf("[%2d][%2d] no. %"PRIu32": force[X]  = %lf\r\n", i_x, i_y, i_part, curr->particle.force[X]);
-                // printf("[%2d][%2d] no. %"PRIu32": force[Y]  = %lf\r\n", i_x, i_y, i_part, curr->particle.force[Y]);
                 curr = curr->next;
             }
         }
@@ -425,14 +463,12 @@ double _kernel_smoothing_viscosity(double distance)
 void _integrate(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double d_time)
 {
     int i_x, i_y;
-    // uint32_t part;
     particle_list_element_t* curr;
     for(i_x = 0; i_x < PARTICLE_GRID_X; ++i_x)
     {
         for(i_y = 0; i_y < PARTICLE_GRID_Y; ++i_y)
         {
             curr = grid[i_x][i_y].particle_list;
-            // for(part = 0; part < grid[i_x][i_y].particle_count; ++part)
             while(curr != NULL)
             {
                 //forward Euler integration
@@ -440,15 +476,11 @@ void _integrate(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
                 {
                     curr->particle.velocity[X] += (d_time * curr->particle.force[X]) / curr->particle.density;
                     curr->particle.velocity[Y] += (d_time * curr->particle.force[Y]) / curr->particle.density;
-                    // printf("[%2d][%2d] no. %"PRIu32": dx  = %lf\r\n", i_x, i_y, part, (d_time * curr->particle.force[X]) / curr->particle.density);
-                    // printf("[%2d][%2d] no. %"PRIu32": dy  = %lf\r\n", i_x, i_y, part, (d_time * curr->particle.force[Y]) / curr->particle.density);
                 }
                 else
                 {
                     curr->particle.velocity[X] += d_time * curr->particle.force[X];
                     curr->particle.velocity[Y] += d_time * curr->particle.force[Y];
-                    // printf("[%2d][%2d] no. %"PRIu32": dx  = %lf\r\n", i_x, i_y, part, d_time * curr->particle.velocity[X]);
-                    // printf("[%2d][%2d] no. %"PRIu32": dy  = %lf\r\n", i_x, i_y, part, d_time * curr->particle.velocity[Y]);
                 }
                 curr->particle.position[X] += d_time * curr->particle.velocity[X];
                 curr->particle.position[Y] += d_time * curr->particle.velocity[Y];
@@ -474,11 +506,61 @@ void _integrate(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], 
                     curr->particle.position[Y] = (PARTICLE_GRID_Y * PARTICLE_GRID_CELL_HEIGHT) - UPPER_BOUNDS_DELTA;
                     curr->particle.velocity[Y] *= BOUNDARY_DAMPENING;
                 }
-                // printf("[%2d][%2d] no. %"PRIu32": position[X]  = %lf\r\n", i_x, i_y, part, curr->particle.position[X]);
-                // printf("[%2d][%2d] no. %"PRIu32": position[Y]  = %lf\r\n", i_x, i_y, part, curr->particle.position[Y]);
                 curr = curr->next;
             }
         }
     }
 }
 
+
+void _integrate_cube(particle_grid_element_t grid[PARTICLE_GRID_X][PARTICLE_GRID_Y], double d_time)
+{
+    int i_x, i_y;
+    particle_list_element_t* curr;
+    for(i_x = 0; i_x < PARTICLE_GRID_X; ++i_x)
+    {
+        for(i_y = 0; i_y < PARTICLE_GRID_Y; ++i_y)
+        {
+            curr = grid[i_x][i_y].particle_list;
+            while(curr != NULL)
+            {
+                //forward Euler integration
+                if(curr->particle.density != 0)
+                {
+                    curr->particle.velocity[X] += (d_time * curr->particle.force[X]) / curr->particle.density;
+                    curr->particle.velocity[Y] += (d_time * curr->particle.force[Y]) / curr->particle.density;
+                }
+                else
+                {
+                    curr->particle.velocity[X] += d_time * curr->particle.force[X];
+                    curr->particle.velocity[Y] += d_time * curr->particle.force[Y];
+                }
+                curr->particle.position[X] += d_time * curr->particle.velocity[X];
+                curr->particle.position[Y] += d_time * curr->particle.velocity[Y];
+
+                //enforce boundary conditions
+                // if(curr->particle.position[X] <= 0 + UPPER_BOUNDS_DELTA)
+                // {
+                //     curr->particle.position[X] = 0 + UPPER_BOUNDS_DELTA;
+                //     curr->particle.velocity[X] *= BOUNDARY_DAMPENING;
+                // }
+                // if(curr->particle.position[X] >= (PARTICLE_GRID_X * PARTICLE_GRID_CELL_WIDTH) - UPPER_BOUNDS_DELTA)
+                // {
+                //     curr->particle.position[X] = (PARTICLE_GRID_X * PARTICLE_GRID_CELL_WIDTH) - UPPER_BOUNDS_DELTA;
+                //     curr->particle.velocity[X] *= BOUNDARY_DAMPENING;
+                // }
+                // if(curr->particle.position[Y] <= 0 + UPPER_BOUNDS_DELTA)
+                // {
+                //     curr->particle.position[Y] = 0 + UPPER_BOUNDS_DELTA;
+                //     curr->particle.velocity[Y] *= BOUNDARY_DAMPENING;
+                // }
+                // if(curr->particle.position[Y] >= (PARTICLE_GRID_Y * PARTICLE_GRID_CELL_HEIGHT) - UPPER_BOUNDS_DELTA)
+                // {
+                //     curr->particle.position[Y] = (PARTICLE_GRID_Y * PARTICLE_GRID_CELL_HEIGHT) - UPPER_BOUNDS_DELTA;
+                //     curr->particle.velocity[Y] *= BOUNDARY_DAMPENING;
+                // }
+                curr = curr->next;
+            }
+        }
+    }
+}
