d2da5520737e052062d87e0497d5d8e782bf6332
Heiko Radde <s76526@beuth-hochschule.de>
2019-03-16 12:13:12 +0100

    moved panels and particle structure into main, added led32x32_init()

--- src/water_led_cube/main.c
+++ src/water_led_cube/main.c
@@ -2,28 +2,36 @@
 #include <led32x32.h>
 #include <utils.h>
 #include <paint_tool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-static panel_t panels[CHAIN_LEN];
-static particle_list_element_t particles[50];
 
 
-int main(int argc, char const *argv[])
+
+int main()
 {
-    (void) argc;
-    (void) argv;
+    double force[3] = {1, 9.8, 1};
+    panel_t panels[CHAIN_LEN];
+    particle_list_element_t particles[50];
+    
+    /* Initialize hardware */
+    led32x32_init();
 
+    /*Initialise simulation*/
     particle_init_list(particles, 50);
     particle_init_grid(panels[0], particles, 50);
-
-
-    double force[2] = {0, 9.8};
+    particle_init_grid(panels[1], NULL, 0);
+    particle_init_grid(panels[2], NULL, 0);
+    particle_init_grid(panels[3], NULL, 0);
+    particle_init_grid(panels[4], NULL, 0);
+    particle_init_grid(panels[5], NULL, 0);
 
     while(1)
     {
         delay(SEC_TO_MSEC(0.1));
         for(int i = 1; i < CHAIN_LEN; ++i)
         {
-            particle_move_single_panel(panels[0], 0.005, force);
+            particle_move_cube(/*top=*/panels[0], /*bottom=*/panels[1], /*front=*/panels[2], /*back=*/panels[3], /*left=*/panels[4], /*right=*/panels[5], 0.005, force);
         }
         lp32x32_refresh_chain(panels);
     }
