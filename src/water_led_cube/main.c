d4e6d7440c7adea5e4c61c405642ff1f7485f24b
Heiko Radde <s76526@beuth-hochschule.de>
2019-04-11 01:30:28 +0200

    water_led_cube/main.c: added switch via P3.7 between local and remote simulation

--- src/water_led_cube/main.c
+++ src/water_led_cube/main.c
@@ -2,10 +2,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define DISPLAY_IT_PER_SIM_IT 50
-#define DT_SIM 0.05
-
-#define ENABLE_RPI 0
+#define RUN_SIM_PER_DISPLAY_REFRESHS          50    ///< Iterations of refreshing the displays until an other step in the simulation is done 
+#define UPDATE_ACC_DATA_PER_DISPLAY_REFRESHS 250    ///< Iterations of refreshing the displays until a new measurement with the accelerometer is done and send to the Raspberry Pi 1B
+#define NO_PARTICLES_LOCAL                    50    ///< Number of particles simulated when local simulation has been enabled
+#define DT_SIM                                 0.05 ///< Timestep of the simulation in sec
 
 #include <utils.h>
 #include <timer.h>
@@ -16,26 +16,127 @@
 #include <accelerometer.h>
 
 
-static panel_t panels[CHAIN_LEN];
-static particle_list_element_t particles[50];
-static uint32_t no_refreshed;
 
-void refresh_display(void)
+
+static panel_t panels[CHAIN_LEN];                               ///< The 6 panels of the cube
+static particle_list_element_t particles[NO_PARTICLES_LOCAL];   ///< All particles simulated
+
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+
+int _sim_on_dev(void)
 {
-    //lp32x32_refresh_chain(panels);
-    //++no_refreshed;
+    double force[3] = {0, 0, 0};
+    int16_t x, y, z;
+    char i2c_msg[16];
+    uint32_t no_refreshed = 0;
+
+    if(protocol_init(panels))
+    {
+        return 1;
+    }
+
+    /*Initialise simulation*/
+    particle_init_list(particles, NO_PARTICLES_LOCAL);
+    particle_init_grid(panels[0], particles, NO_PARTICLES_LOCAL);
+    particle_init_grid(panels[1], NULL, 0);
+    particle_init_grid(panels[2], NULL, 0);
+    particle_init_grid(panels[3], NULL, 0);
+    particle_init_grid(panels[4], NULL, 0);
+    particle_init_grid(panels[5], NULL, 0);
+    
+
+    while(1)
+    {
+
+        if(no_refreshed >= RUN_SIM_PER_DISPLAY_REFRESHS)
+        {
+            x = getX();
+            y = getY();
+            z = getZ();
+            force[0] = ((x * 1.0) / 128.0) * 9.8;
+            force[1] = ((y * 1.0) / 128.0) * 9.8;
+            force[2] = ((z * 1.0) / 128.0) * 9.8;
+            sprintf(i2c_msg, "%4.2f %4.2f %4.2f", force[0], force[1], force[2]);
+            lcd_print_coordinates((unsigned char *)i2c_msg);
+
+            acc_data_t acc_data = {.force_x = force[0],\
+                                   .force_y = force[1],\
+                                   .force_z = force[2]};
+            protocol_send_acc_data(acc_data);
+            
+            particle_move_cube(/*top=*/panels[0], /*bottom=*/panels[1], /*front=*/panels[2], /*back=*/panels[3], /*left=*/panels[4], /*right=*/panels[5], DT_SIM, force);
+            no_refreshed = 0;
+        }
+        lp32x32_refresh_chain_blue(panels);
+        ++no_refreshed;
+    }
+    
+    return 0;
 }
+
+int _sim_external(void)
+{
+    double force[3] = {0, 0, 0};
+    int16_t x, y, z;
+    char i2c_msg[16];
+    uint32_t no_refreshed = 0;
+
+    if(protocol_init(panels))
+    {
+        return 1;
+    }
+
+    /*Initialise simulation*/
+    particle_init_list(particles, 50);
+    particle_init_grid(panels[0], NULL, 0);
+    particle_init_grid(panels[1], NULL, 0);
+    particle_init_grid(panels[2], NULL, 0);
+    particle_init_grid(panels[3], NULL, 0);
+    particle_init_grid(panels[4], NULL, 0);
+    particle_init_grid(panels[5], NULL, 0);
+    
+
+    while(1)
+    {
+
+        if(no_refreshed >= UPDATE_ACC_DATA_PER_DISPLAY_REFRESHS)
+        {
+            x = getX();
+            y = getY();
+            z = getZ();
+            force[0] = ((x * 1.0) / 128.0) * 9.8;
+            force[1] = ((y * 1.0) / 128.0) * 9.8;
+            force[2] = ((z * 1.0) / 128.0) * 9.8;
+            sprintf(i2c_msg, "%4.2f %4.2f %4.2f", force[0], force[1], force[2]);
+            lcd_print_coordinates((unsigned char *)i2c_msg);
+
+            acc_data_t acc_data = {.force_x = force[0],\
+                                   .force_y = force[1],\
+                                   .force_z = force[2]};
+            protocol_send_acc_data(acc_data);
+            
+            no_refreshed = 0;
+        }
+        lp32x32_refresh_chain_24bit_rgb(panels);
+        ++no_refreshed;
+    }
+    
+    return 0;
+}
+
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
 
 
 int main()
 {
-    double force[3] = {1, 9.8, 1};
     volatile uint8_t id;
-    char i2c_msg[16];
-    int16_t x;
-    int16_t y;
-    int16_t z;
-    no_refreshed = 0;
     
     CCLKCFG = 0x03; // 0x3 => generates 72 MHz (Max.)
     
@@ -61,48 +162,17 @@ int main()
     lcd_print_message((unsigned char *)"ADXL345 started!");
     delay(2000);
 
-    if(protocol_init(panels))
+
+    if((FIO3PIN0 & 0x80) != 0)
     {
-        return 1;
+        lcd_print_message((unsigned char *)"Local Simulation");
+        delay(2000);
+        return _sim_on_dev();
     }
-
-    /*Initialise simulation*/
-    particle_init_list(particles, 50);
-    //particle_init_grid(panels[0], particles, 50);
-    particle_init_grid(panels[0], NULL, 0);
-    particle_init_grid(panels[1], NULL, 0);
-    particle_init_grid(panels[2], NULL, 0);
-    particle_init_grid(panels[3], NULL, 0);
-    particle_init_grid(panels[4], NULL, 0);
-    particle_init_grid(panels[5], NULL, 0);
-    
-    //init_timer(/*timer_num=*/1, /*timer_isr_t=*/refresh_display);
-
-    while(1)
+    else
     {
-
-        if(no_refreshed >= DISPLAY_IT_PER_SIM_IT)
-        {
-            x = getX();
-            y = getY();
-            z = getZ();
-            force[0] = ((x * 1.0) / 128.0) * 9.8;
-            force[1] = ((y * 1.0) / 128.0) * 9.8;
-            force[2] = ((z * 1.0) / 128.0) * 9.8;
-            sprintf(i2c_msg, "%4.2f %4.2f %4.2f", force[0], force[1], force[2]);
-            lcd_print_coordinates((unsigned char *)i2c_msg);
-
-            acc_data_t acc_data = {.force_x = force[0],\
-                                   .force_y = force[1],\
-                                   .force_z = force[2]};
-            protocol_send_acc_data(acc_data);
-            
-            // particle_move_cube(/*top=*/panels[0], /*bottom=*/panels[1], /*front=*/panels[2], /*back=*/panels[3], /*left=*/panels[4], /*right=*/panels[5], DT_SIM, force);
-            no_refreshed = 0;
-        }
-        lp32x32_refresh_chain_24bit_rgb(panels);
-        ++no_refreshed;
+        lcd_print_message((unsigned char *)"Remote Simulation");
+        delay(2000);
+        return _sim_external();
     }
-
-    return 0;
 }
